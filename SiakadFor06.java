c77a852bcd00416441c8266a45d6ed9eb3d537ed
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-10-22 12:53:26 +0700

    modifikasiSiakadFor

--- SiakadFor06.java
+++ SiakadFor06.java
@@ -4,6 +4,7 @@ public class SiakadFor06 {
         Scanner sc = new Scanner (System.in);
 
        double nilai, tertinggi = 0, terendah = 100;
+       int jumlahMahasiswaLulus = 0,jumlahMahasiswaTidakLulus=0;
 
        for (int i = 1; i <= 10; i++) {
            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,10 +16,18 @@ public class SiakadFor06 {
            if (nilai < terendah) {
                terendah = nilai;
            }
-
+           if (nilai >= 60) {
+              jumlahMahasiswaLulus++;
+           }
+           else  {
+               jumlahMahasiswaTidakLulus++;
        }
+       
+     } 
        System.out.println("Nilai tertinggi: " + tertinggi);
        System.out.println("Nilai terendah: " + terendah);
-   }
+       System.out.println("Jumlah mahasiswa yang lulus: " + jumlahMahasiswaLulus);
+       System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahMahasiswaTidakLulus);        
+    }
 
 }
